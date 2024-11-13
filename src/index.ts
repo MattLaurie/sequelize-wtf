d1c32dc7b3ef57351ce890a8f56522eb23709774
Matt Laurie <MattLaurie@users.noreply.github.com>
2024-11-13 16:09:46 +1000

    better default

--- src/index.ts
+++ src/index.ts
@@ -28,69 +28,9 @@ const setup = async () => {
         },
     });
 
-    const ThingFields = sequelize.define('ThingFields', {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-        },
-        ThingId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'Things',
-                key: 'id',
-            }
-        },
-        fields: {
-            type: DataTypes.JSON,
-            allowNull: false
-        },
-        createdAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
-            defaultValue: sequelize.fn('NOW'),
-        },
-        updatedAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
-            defaultValue: sequelize.fn('NOW'),
-        },
-    });
-
-    Thing.hasMany(ThingFields);
-    ThingFields.belongsTo(Thing);
-
     await sequelize.sync({force: true});
 
-    const models = await Thing.findAll({
-        include: [
-            {
-                model: ThingFields,
-                attributes: ['id', 'fields'],
-                where: {
-                    'fields.brand': 'spt'
-                },
-            }
-        ],
-        order: [
-            [
-                sequelize.fn(
-                    'JSON_UNQUOTE',
-                    sequelize.fn(
-                        'JSON_EXTRACT',
-                        sequelize.col('ThingFields.fields'),
-                        sequelize.literal('\'$.\\"brand\\"\'')
-                    )
-                ),
-                'DESC'
-            ]
-            // [
-            //     sequelize.literal('JSON_UNQUOTE(JSON_EXTRACT(`ThingFields`.`fields`, \'$."brand"\'))'),
-            //     'DESC'
-            // ]
-        ]
-    });
+    const models = await Thing.findAll({});
 }
 
 (async () => {
