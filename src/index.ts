39ad7c8d914fb537bf7914c07f605ba5ef89fecc
Matt Laurie <MattLaurie@users.noreply.github.com>
2024-12-28 06:02:37 +1000

    added more options

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,40 @@
-import 'dotenv/config';
-import {DataTypes, Sequelize} from 'sequelize';
+import "dotenv/config";
+import { DataTypes, Sequelize } from "sequelize";
 
-const setup = async () => {
-    const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: process.env.DATABASE_HOSTNAME,
-        database: process.env.DATABASE_NAME,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-    });
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  host: process.env.DATABASE_HOSTNAME,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined,
+  logging: true
+});
 
-    const Thing = sequelize.define('Thing', {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-        },
-        createdAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
-            defaultValue: sequelize.fn('NOW'),
-        },
-        updatedAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
-            defaultValue: sequelize.fn('NOW'),
-        },
-    });
+const Thing = sequelize.define("Thing", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    defaultValue: sequelize.fn("NOW"),
+  },
+  updatedAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    defaultValue: sequelize.fn("NOW"),
+  },
+});
 
-    await sequelize.sync({force: true});
-
-    const models = await Thing.findAll({});
-}
+const stuff = async () => {
+  const models = await Thing.findAll({});
+};
 
 (async () => {
-    await setup();
-    process.exit(0);
+  await sequelize.sync({ force: true });
+  await stuff();
+  process.exit(0);
 })();
