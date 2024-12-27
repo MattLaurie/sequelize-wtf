47c21ba0d8ddb9c955153fc3af3143ab6764dcde
Matt Laurie <MattLaurie@users.noreply.github.com>
2024-12-28 06:46:41 +1000

    basic postgres

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import 'dotenv/config';
 import { DataTypes, Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
-  dialect: 'mysql',
+  dialect: 'postgres',
   host: process.env.DATABASE_HOSTNAME,
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined,
-  logging: true,
 });
 
 const Thing = sequelize.define('Thing', {
